3c53d2194412f6f4ba89e5a2c27429fcbbcda3f2
Du Toit Spies <spsdut001@myuct.ac.za>
2022-08-29 21:10:22 +0200

    More benchmarks

--- Image/putpixel/putpixel.py
+++ Image/putpixel/putpixel.py
@@ -5,13 +5,14 @@ from timeit import default_timer as dt
 impl = sys.implementation.name
 api = sys.argv[1]
 
-im = Image.new('RGBA', (400,400))
 out = open("output/"+impl+"_"+api, "w")
+im = Image.new("RGB",(400,400))
 
 for i in range(100):
     start = dt()
-    for j in range(500):
-        im2 = im.copy()
-    out.write(str(dt() - start) + "\n")
+    for j in range(400):
+        for k in range(400):
+            im.putpixel((j,k),(i,i,i))
+    out.write(str(dt()-start)+"\n")
 
 out.close()
